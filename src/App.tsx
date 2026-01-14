4209b3fff4504f197316e843bcf1bbd3474df17e
EwerssonSllv <ewerssonsllv@gmail.com>
2026-01-14 17:58:55 -0300

    Tailwind, Routes, Outlet, Toast, Layout and Header created and added

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from "react-router";
+import { Home } from "./pages/home";
+import { Register } from "./pages/register";
+import { Login } from "./pages/login";
+import { New } from "./pages/dashboard/new";
+import { Dashboard } from "./pages/dashboard";
+import { Detail } from "./pages/car";
+import { Layout } from "./pages/components/layout";
+
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/car/:id",
+        element: <Detail />
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />
+      },
+      {
+        path: "/dashboard/new",
+        element: <New />
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  }
+])
+
+export { router }
