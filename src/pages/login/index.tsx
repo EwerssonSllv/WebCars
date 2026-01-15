41bd85aba68b19cd0c2b9cdca1fd9f539184d395
EwerssonSllv <ewerssonsllv@gmail.com>
2026-01-15 17:16:48 -0300

    React-Hook=Form and Zod added

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,24 @@
-export function Login(){
-    return(
-        <div>
-            <h1>Login Page!</h1>
-        </div>
+import { Link } from 'react-router'
+import logoImg from '../../assets/logo.svg'
+import { Container } from '../../components/container'
+import { Input } from '../../components/input'
+
+export function Login() {
+    return (
+        <Container>
+            <div className='w-full min-h-screen flex justify-center items-center flex-col gap-4'>
+                <Link className='mb-6 max-w-sm w-full' to="/">
+                    <img className='w-full' src={logoImg} alt="Logo" />
+                </Link>
+
+                <form className='bg-white max-w-xl w-full rounded-lg'>
+                    <Input
+                        type="email"
+                        placeholder="ex: user@gmail.com..."
+                        name="email"
+                    />
+                </form>
+            </div>
+        </Container>
     )
 }
